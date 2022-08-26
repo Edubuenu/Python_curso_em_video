6457a6869191a88cbb352041192ff4fb7a9418fc
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2022-08-26 10:09:09 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,11 @@
+n = input('Digite algo:')
+print('O tipo primitivo desse valor é', type(n))
+print('Só tem espaços?', n.isspace())
+print('É um número?', n.isnumeric())
+print('É alfabético?', n.isalpha())
+print('É alfanúmerico?', n.isalnum())
+print('Está em maiúscula?', n.isupper())
+print('Está em minuscula?', n.islower())
+print('Está em capitalizada?', n.istitle())
+
+      
