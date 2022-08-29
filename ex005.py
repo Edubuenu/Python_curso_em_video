45acfc93a12f62677c03d92f3c5041b43ef97b5e
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2022-08-29 10:42:02 -0300

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Digite um número inteiro:'))
+s = n - 1
+a = n + 1
+print(' O sucessor é {} \n O antecessor é {}'.format(s, a))
---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número inteiro:'))
+d = n * 2
+t = n * 3
+r = n ** 2
+print(' O dobro é {} \n O triplo é {} \n A raiz quadrada é {}'.format( d, t, r))
