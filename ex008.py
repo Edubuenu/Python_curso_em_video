4cc4d6aa2e20a9ca05ebff2efaa00c07f1c330ef
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2022-08-30 09:23:50 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,4 @@
+m = int(input('Digite o valor em metros:'))
+c = m *100
+mi = m * 1000
+print(' O valor em metros é {} \n O valor em centimetros é {} \n O valor em milimitros é {}'.format(m, c, mi))
