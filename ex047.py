eabcd4f284035f7134a7a71c5a4420de3b8d0423
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2022-12-13 10:33:27 -0300

    Add files via upload

---
+++ ex047.py
@@ -0,0 +1,4 @@
+print('\033[31mOs números pares de 1 a 50, são:\033[33m')
+for cont in range(0,51,2):
+    print(cont, end=' ')
+
---
+++ ex048.py
@@ -0,0 +1,5 @@
+soma = 0
+for c in range(0, 500):
+    if c % 3 == 0:
+        soma = soma + c
+print('A soma de todos valores é {}'.format(soma))
